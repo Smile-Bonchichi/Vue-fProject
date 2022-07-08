af498673a653e486e8c32fbce0828c4818568cd9
Nurdin <ulanovnurdin@gmail.com>
2022-07-09 02:21:24 +0600

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app-main-layout">
+
+    <!--    <Navbar @click="isOpen = !isOpen"/>-->
+    <Navbar/>
+
+    <Sidebar v-model="isOpen"/>
+
+    <main class="app-content" :class="{full: !isOpen}">
+      <div class="app-page">
+        <router-view/>
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <router-link class="btn-floating btn-large blue"
+                   to="/record">
+        <i class="large material-icons">add</i>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/app/NavbarComponent'
+import Sidebar from '@/components/app/SidebarComponent'
+
+export default {
+  name: 'MainLayout',
+  data: () => ({
+    isOpen: true
+  }),
+  computed: {},
+  components: {
+    Navbar,
+    Sidebar
+  }
+}
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,66 @@
+<template>
+  <form class="card auth-card" @submit.prevent="submitHandler">
+    <div class="card-content">
+      <span class="card-title">CRM бухгалтерия</span>
+      <div class="input-field">
+        <input
+            id="email"
+            type="text"
+            class="validate"
+        >
+        <label for="email">Email</label>
+        <small
+            class="helper-text invalid"
+        >Email</small>
+      </div>
+      <div class="input-field">
+        <input
+            id="password"
+            type="password"
+            class="validate"
+        >
+        <label for="password">Password</label>
+        <small class="helper-text invalid">Password</small>
+      </div>
+    </div>
+    <div class="card-action">
+      <div>
+        <button class="btn waves-effect waves-light auth-submit" type="submit">
+          Войти
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
+
+      <p class="center">
+        Нет аккаунта?
+        <router-link to="/register">Зарегистрироваться</router-link>
+      </p>
+    </div>
+  </form>
+</template>
+
+<script>
+import messagePlugin from "@/utils/message.plugin";
+import messages from "@/utils/messages";
+
+export default {
+  name: 'LoginView',
+  data: () => ({
+    email: '',
+    password: ''
+  }),
+  mounted() {
+    if (messages[this.$route.query.message]) {
+      messagePlugin.message(messages[this.$route.query.message])
+    }
+  },
+  methods: {
+    async submitHandler() {
+      const formData = {
+        email: this.email,
+        password: this.password
+      }
+    }
+  }
+}
+</script>
